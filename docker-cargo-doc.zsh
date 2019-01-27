4cb6092d29264b044987a211d21fc135cc83f45f
ncaq <ncaq@ncaq.net>
2019-01-27 18:44:45 +0900

    added: docker-cargo-doc.zsh

--- docker-cargo-doc.zsh
+++ docker-cargo-doc.zsh
@@ -6,4 +6,4 @@ mkdir -p target/doc
 
 cp -a ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/share/doc/rust/html/* target/doc/
 
-docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp rust:latest -- cargo doc --all
+docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/rust-crate-acme-everything -w /usr/src/rust-crate-acme-everything rust:latest cargo doc --all
